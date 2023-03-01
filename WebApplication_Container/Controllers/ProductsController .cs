e13e0df83087f1b4449c36b0aabff8852fd18d8c
folken919 <adrianrengifo@gmail.com>
2023-03-01 16:48:24 -0500

    Add files via upload

---
+++ WebApplication_Container/Controllers/ProductsController .cs
@@ -0,0 +1,44 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using WebApplication_Container.Models;
+
+namespace WebApplication_Container.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : Controller
+{
+    Product[] products = new Product[]
+    {
+            new Product
+            {
+                Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1
+            },
+            new Product
+            {
+                Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M
+            },
+            new Product
+            {
+                Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M
+            }
+    };
+
+    [HttpGet]
+    public IEnumerable<Product> GetAllProducts()
+    {
+        return products;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Product> GetProduct(int id)
+    {
+        var product = products.FirstOrDefault((p) => p.Id == id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        return product;
+    }
+}
